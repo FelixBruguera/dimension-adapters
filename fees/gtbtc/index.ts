1e9887e0928e07e41e2772ee4d554f20eefb4d5f
Felix Bruguera <felixbruguera0@gmail.com>
2025-12-11 16:04:02 -0400

    add supply

--- fees/gtbtc/index.ts
+++ fees/gtbtc/index.ts
@@ -3,37 +3,34 @@ import { FetchOptions, SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
 import { METRIC } from "../../helpers/metrics";
 import coreAssets from "../../helpers/coreAssets.json";
+import { getPrices, getMcaps } from "@defillama/sdk/build/util/coins";
 
-const evmAddress = '0xc2d09CF86b9ff43Cb29EF8ddCa57A4Eb4410D5f3'
-const solanaAddress = 'gtBTCGWvSRYYoZpU9UZj6i3eUGUpgksXzzsbHk2K9So'
-const evmChains = [CHAIN.ETHEREUM, CHAIN.BSC, CHAIN.BASE]
+
+const gBTC = '0xc2d09CF86b9ff43Cb29EF8ddCa57A4Eb4410D5f3'
+const coingeckoId = 'coingecko:gate-wrapped-btc'
+const wbtc = coreAssets.ethereum.WBTC
 
 const exchangeRateUpdatedAbi = 'event ExchangeRateUpdated(uint256 oldRate, uint256 newRate, address indexed updater)'
 
 const fetch = async (_t: number, _c: any, options: FetchOptions) => {
     const dailyFees = options.createBalances();
+    const supplySideRevenue = options.createBalances();
     let growthRate = 0
-    let totalSupply = 0
+    const [gtBTCPrice, gtBTCMcap] = await Promise.all([
+        getPrices([coingeckoId], 'now'),
+        getMcaps([coingeckoId], 'now')
+    ])
+    const gtBTCSupply = gtBTCMcap[coingeckoId].mcap/gtBTCPrice[coingeckoId].price
     const exchangeRateLog = await options.getLogs({
         eventAbi: exchangeRateUpdatedAbi,
-        target: evmAddress
+        target: gBTC
     })
     exchangeRateLog.forEach(log => {
-        console.log(log)
-        growthRate = (log.newRate - log.oldRate) / 1e6
+        growthRate = (Number(log.newRate) - Number(log.oldRate))
     })
-    console.log(exchangeRateLog)
-    console.log(growthRate)
-    const calls = evmChains.map(chain => {
-        return {
-            chain: chain,
-            target: evmAddress
-        }})
-    const call = await options.api.multiCall({
-            calls: calls,
-            abi: 'erc20:totalSupply'
-        })
-    console.log(call)
+    const fees = (growthRate * gtBTCSupply) * 1e2
+    dailyFees.add(wbtc, fees, METRIC.STAKING_REWARDS)
+    supplySideRevenue.add(wbtc, fees, METRIC.STAKING_REWARDS)
     return {
         dailyFees,
         dailyRevenue: options.createBalances()
@@ -41,8 +38,8 @@ const fetch = async (_t: number, _c: any, options: FetchOptions) => {
 }
 
 const methodology= {
-    Fees: "Redeem fees + yield distribution",
-    UserFees: "Redeem fees from users",
+    Fees: "Yield generated from staking",
+    SupplySideRevenue: "Staking rewards distributed",
   }
 
 const adapter: SimpleAdapter = {
