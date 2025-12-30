30037552d18a4a2686514d428f93dd3afde2825e
Felix Bruguera <felixbruguera0@gmail.com>
2025-12-30 15:07:18 -0400

    finish

--- dexs/seer/index.ts
+++ dexs/seer/index.ts
@@ -1,73 +1,36 @@
 import { CHAIN } from "../../helpers/chains";
 import { Chain, FetchOptions, SimpleAdapter } from "../../adapters/types";
-import * as sdk from "@defillama/sdk";
 import { ethers } from "ethers";
-import coreAssets from "../../helpers/coreAssets.json"
-import { addOneToken } from "../../helpers/prices";
+import { BytesLike } from "ethers";
 
 interface chainAddressInterface {
     conditionalTokens: string
-    poolFactory: string,
-    poolCreatedAbi: string,
-    swapAbi: string
-}
-interface poolIface {
-    pool: string,
-    sDaiPosition: string
+    router?: string,
 }
 
 const chainAddresses : Record<Chain, chainAddressInterface> = {
     [CHAIN.XDAI]: {
         conditionalTokens: "0xCeAfDD6bc0bEF976fdCd1112955828E00543c0Ce",
-        poolFactory: "0xA0864cCA6E114013AB0e27cbd5B6f4c8947da766",
-        poolCreatedAbi: "event Pool(address indexed token0, address indexed token1, address pool)",
-        swapAbi: "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 price, uint128 liquidity, int24 tick)",
+        router: "0xeC9048b59b3467415b1a38F63416407eA0c70fB8",
     },
     [CHAIN.ETHEREUM]: {
         conditionalTokens: "0xC59b0e4De5F1248C1140964E0fF287B192407E0C",
-        poolFactory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-        poolCreatedAbi: "event PoolCreated(address indexed token0, address indexed token1, uint24 indexed fee, int24 tickSpacing, address pool)",
-        swapAbi: "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)"
+        router: "0x886Ef0A78faBbAE942F1dA1791A8ed02a5aF8BC6"
     }
 }
-
-const sDai = coreAssets.xdai.sDAI
 const positionSplit = "event PositionSplit(address indexed stakeholder, address collateralToken, bytes32 indexed parentCollectionId, bytes32 indexed conditionId, uint256[] partition, uint256 amount)"
 const positionMerge = "event PositionsMerge(address indexed stakeholder, address collateralToken, bytes32 indexed parentCollectionId, bytes32 indexed conditionId, uint256[] partition, uint256 amount)"
 
 async function fetch(options: FetchOptions) {
-    const { conditionalTokens, poolFactory, poolCreatedAbi, swapAbi } = chainAddresses[options.chain]
-    const cacheKey = `tvl-adapter-cache/cache/logs/${options.chain}/${poolFactory.toLowerCase()}.json`
-    const { logs } = await sdk.cache.readCache(cacheKey, { readFromR2Cache: true})
-    const poolCreateIface = new ethers.Interface([poolCreatedAbi])
-    const pools: string[] = []
-    const otherTokens: string[] = []
-    const poolsWithTokens = {}
-    logs.forEach((log: any) => {
-        const args = poolCreateIface.parseLog(log)?.args
-        pools.push(args?.pool)
-        if (args?.token0.toLowerCase() === sDai.toLowerCase()) {
-            otherTokens.push(args.token1)
-            poolsWithTokens[args?.pool.toLowerCase()] = { token0: args.token0, token1: args.token1}
-        }
-        else {
-            otherTokens.push(args?.token0)
-            poolsWithTokens[args?.pool.toLowerCase()] = { token0: args.token0, token1: args.token1}
-        }
-    })
-    const multiTokens = await options.api.multiCall({ abi: "address:multiToken", calls: otherTokens, permitFailure: true})
-    const filteredPools = pools.filter((_, i) => multiTokens[i]?.toLowerCase() === conditionalTokens.toLowerCase())
+    const { conditionalTokens, router } = chainAddresses[options.chain]
     const dailyVolume = options.createBalances()
 
-    const [splits, merges, swaps] = await Promise.all([
-        options.getLogs({ target: conditionalTokens, eventAbi: positionSplit }),
-        options.getLogs({ target: conditionalTokens, eventAbi: positionMerge }),
-        options.getLogs({ targets: filteredPools, eventAbi: swapAbi, entireLog: true})
+    const [splits, merges] = await Promise.all([
+        options.getLogs({ target: conditionalTokens, eventAbi: positionSplit, topics: ["0x2e6bb91f8cbcda0c93623c54d0403a43514fabc40084ec96b6d5379a74786298", ethers.zeroPadValue(router as BytesLike, 32), null as any, null as any] }),
+        options.getLogs({ target: conditionalTokens, eventAbi: positionMerge, topics: ["0x6f13ca62553fcc2bcd2372180a43949c1e4cebba603901ede2f4e14f36b282ca", ethers.zeroPadValue(router as BytesLike, 32), null as any, null as any] }),
     ])
-    splits.concat(merges).forEach(log => dailyVolume.add(log.collateralToken, log.amount))
-    swaps.forEach(log => {
-        const { token0, token1 } = poolsWithTokens[log.address.toLowerCase()]
-        addOneToken({ chain: options.chain, balances: dailyVolume, token0, token1, amount0: log.args.amount0, amount1: log.args.amount1 })
+    splits.concat(merges).forEach(log => {
+        dailyVolume.add(log.collateralToken, log.amount)
     })
 
     return {
@@ -78,8 +41,8 @@ async function fetch(options: FetchOptions) {
 const adapter : SimpleAdapter = {
     version: 2,
     fetch,
-    start: "202-01-01",
-    chains: [CHAIN.XDAI]
+    start: "2024-08-04",
+    chains: [CHAIN.XDAI, CHAIN.ETHEREUM]
 }
 
 export default adapter
